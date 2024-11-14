d63f004f04f509afd820195c8eb791d000a40742
nicdia <nicodia99@gmail.com>
2024-11-14 18:25:37 +0100

    Not yet finished with data processing.

--- data_processing/main_data_processing.py
+++ data_processing/main_data_processing.py
@@ -1,6 +1,6 @@
-from data_processing.attribute_filtering import filter_and_create_table, handle_conf_attrfilter, custom_ALKIS_building_filtering
-from data_processing.join_with_alkis import handle_conf_join_with_alkis, join_POI_with_ALKIS, rename_fields_with_schema
-from misc.util_fcts import setup, get_logging
+from attribute_filtering import filter_and_create_table, handle_conf_attrfilter, custom_ALKIS_building_filtering
+from join_with_alkis import handle_conf_join_with_alkis, join_POI_with_ALKIS, rename_fields_with_schema
+from util_fcts import connect2DB
 import json
 import os
 
@@ -8,9 +8,10 @@ import os
 
 def main_processing():
     # set up
-    config_path = os.path.join("data_processing", "config_data_processing.json")
-    db_con, config = setup(config_path)
-    logger = get_logging()
+    with open("C:\\Master\\GeoinfoPrj_Sem1\\Erreichbarkeitsanalyse\\data_processing\\config_data_processing.json", "r") as file:
+            config = json.load(file)
+    db_con = connect2DB()
+
     
     # 1. filter by field values
     attr_filter = handle_conf_attrfilter(config)
@@ -18,11 +19,12 @@ def main_processing():
         filter_and_create_table(attr_filter, db_con)
         custom_ALKIS_building_filtering(db_con)
 
-    # 2. rename fields so there will be no error when joining
     join_config = handle_conf_join_with_alkis(config)
-    #3. spatial join der POIs mit alkis daten
+
     if join_config:
+        # 2. rename fields so there will be no error when joining
         rename_fields_with_schema(db_con, join_config)
+        #3. spatial join der POIs mit alkis daten
         #join_POI_with_ALKIS(db_con, join_config) 
 
     #3. data source mismatch handling (delete osm features)
@@ -34,5 +36,5 @@ def main_processing():
     # --> result: one dataset for every schema, with only polygons that match flurstuecke/gebauede
 
 
-
+main_processing()
 
